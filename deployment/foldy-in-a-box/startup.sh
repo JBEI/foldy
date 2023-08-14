1edaa81d0cb5681d8a45ba4cca3e08b18bf6a028
Jacob Roberts <jacoberts@gmail.com>
2023-08-14 14:05:20 -0700

    Try fixing url for foldy.

--- deployment/foldy-in-a-box/startup.sh
+++ deployment/foldy-in-a-box/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 MY_URL=`curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`
+echo "MY_URL: $MY_URL"
+FOLDY_BOX_URL=$MY_URL echo "FOLDY_BOX_URL: $FOLDY_BOX_URL"
 FOLDY_BOX_URL=$MY_URL /usr/bin/docker compose -f foldy-in-a-box.yml build
 FOLDY_BOX_URL=$MY_URL /usr/bin/docker compose -f foldy-in-a-box.yml up -d --force-recreate
