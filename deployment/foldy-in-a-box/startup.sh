d51261194998fa8ccfe654c1b5234fb2cdc60637
Jacob Roberts <jacoberts@gmail.com>
2023-08-14 14:15:39 -0700

    Make sure DBs are installed when running Foldy In a Box.

--- deployment/foldy-in-a-box/startup.sh
+++ deployment/foldy-in-a-box/startup.sh
@@ -3,5 +3,12 @@
 MY_URL=`curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip`
 echo "MY_URL: $MY_URL"
 FOLDY_BOX_URL=$MY_URL echo "FOLDY_BOX_URL: $FOLDY_BOX_URL"
+
+# Rebuild the images with the URL.
 FOLDY_BOX_URL=$MY_URL /usr/bin/docker compose -f foldy-in-a-box.yml build
+
+# Start the service.
 FOLDY_BOX_URL=$MY_URL /usr/bin/docker compose -f foldy-in-a-box.yml up -d --force-recreate
+
+# Make sure DBs are installed.
+FOLDY_BOX_URL=$MY_URL /usr/bin/docker compose -f foldy-in-a-box.yml exec backend flask db upgrade
